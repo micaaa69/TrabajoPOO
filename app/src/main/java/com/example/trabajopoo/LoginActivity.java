6d19bd83f92fa2b8f51fef63c315ffb204ccf3be
William Valencia <willimaca09ac@hotmail.com>
2022-11-19 17:03:48 -0500

    Diseño del menu completado

--- app/src/main/java/com/example/trabajopoo/LoginActivity.java
+++ app/src/main/java/com/example/trabajopoo/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -22,10 +23,11 @@ public class LoginActivity extends AppCompatActivity {
     AutoCompleteTextView autoCompleteTxt, autoCompleteTxt_2;
 
     String[] facultades={"Ingeniería","Empresas","Derecho","Comunicaciones","Humanidades","Educación"};
-    String[] profesiones={"Profesor","Alumno","Administrativo"};
+    //String[] profesiones={"Profesor","Alumno","Administrativo"};
     String facultad;
-    String profesion;
-    EditText userName, fecha;
+    //String profesion;
+    EditText userName;
+    TextView fecha;
     ImageButton btn_calendario;
     Button btnIngresar;
 
@@ -37,8 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         //relacionar los componentes gráficos con los lógicos
 
         autoCompleteTxt=findViewById(R.id.auto_complete_txt);
-        autoCompleteTxt_2=findViewById(R.id.auto_complete_txt_2);
-        userName=findViewById(R.id.et_user);
+        //autoCompleteTxt_2=findViewById(R.id.auto_complete_txt_2);
         fecha=findViewById(R.id.et_date);
         btn_calendario=findViewById(R.id.btn_calendario);
         btnIngresar=findViewById(R.id.button3);
@@ -78,6 +79,37 @@ public class LoginActivity extends AppCompatActivity {
 
         });
 
+        fecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog dialog = new DatePickerDialog(LoginActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month=month+1;
+                        String date = null;
+                        if (month>9 && dayOfMonth>9){
+                            date=dayOfMonth+"/"+month+"/"+year;
+                        }
+                        if (month>9 && dayOfMonth<10){
+                            date="0"+dayOfMonth+"/"+month+"/"+year;
+                        }
+                        if (month<10 && dayOfMonth<10){
+                            date="0"+dayOfMonth+"/0"+month+"/"+year;
+                        }
+                        if (month<10 && dayOfMonth>9){
+                            date=+dayOfMonth+"/0"+month+"/"+year;
+                        }
+                        fecha.setText(date);
+                    }
+                },year,month,day);
+                dialog.show();
+            }
+
+        });
+
+
+
+
 
         ArrayAdapter<String>  adapterItems= new ArrayAdapter<String>(this,R.layout.list_item,facultades);
 
@@ -90,25 +122,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        ArrayAdapter<String> adapterItems_2=new ArrayAdapter<String>(this,R.layout.list_item,profesiones);
 
-        autoCompleteTxt_2.setAdapter(adapterItems_2);
-
-        autoCompleteTxt_2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-                profesion=parent.getItemAtPosition(i).toString();
-            }
-
-
-        });
-        btnIngresar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, Pregunta1.class);
-                startActivity(intent);
-            }
-        });
+//        btnIngresar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(LoginActivity.this, Pregunta1.class);
+//                startActivity(intent);
+//            }
+//        });
 
     }
 
@@ -117,18 +138,14 @@ public class LoginActivity extends AppCompatActivity {
         String nombre=userName.getText().toString();
         String fecha_s=fecha.getText().toString();
 
-        if (nombre.equals("")){
-            Toast error= Toast.makeText(this, "Ingrese su nombre",Toast.LENGTH_LONG);
-            error.show();
-        }
 
         if (fecha_s.equals("")){
             Toast error= Toast.makeText(this, "Ingrese la fecha",Toast.LENGTH_LONG);
             error.show();
         } //falta
 
-        if (!nombre.equals("")&&!fecha_s.equals("")){
-            Intent iIngresarPreguntas = new Intent(this, MainActivity.class);
+        if (!fecha_s.equals("")){
+            Intent iIngresarPreguntas = new Intent(this, Pregunta1.class);
             iIngresarPreguntas.putExtra("userName",nombre);
             startActivity(iIngresarPreguntas);
         }
